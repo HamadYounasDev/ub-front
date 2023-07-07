fb8880bd63557930188fa3cfd304ccdc83301d76
dhrp099 <shahzaib@dhrp.com.au>
2023-07-07 15:09:57 +0500

    Removed Console and Fixed Console Errors

--- src/components/FreelancerSelector.jsx
+++ src/components/FreelancerSelector.jsx
@@ -5,22 +5,20 @@ import { MyContext } from "../contexts/DestinationDetails";
 
 const FreelancerSelector = () => {
   const { selectedHotel, setSelectedFreelancer, destinationDetails, selectedFreelancer } = useContext(MyContext);
-  console.log("selected Hotel", selectedHotel);
   const [confirmationModal, setConfirmationModal] = useState(false);
   const handlerFreelancerSelection = (data) => {
     setSelectedFreelancer(data)
     setConfirmationModal(true);
   };
 
-  console.log('selected Freelancer',selectedFreelancer)
 
   return (
-    <div class="bg-white">
-      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900 mb-10">
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-10">
           Freelancer
         </h2>
-        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {destinationDetails?.ACF?.related_freelancers?.map((item) => (
             <SingleItem
               key={item.id}
